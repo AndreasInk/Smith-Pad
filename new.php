55828839f47fee17c0a5211801dbd822094b81bf
senalbulumulle <senal.bulumulle@gmail.com>
2022-05-26 21:56:05 -0400

    Re-organizing code with PHP (part-1)

---
+++ new.php
@@ -0,0 +1,18 @@
+<!-- 
+    index.php
+
+    Subjects is a feature of the Smith-Pad Web-Interface that enables students 
+    to learn Math, Reading, Science, and other subjects in a fun and tactful 
+    manner, even if they are offline. 
+-->
+
+<?php include './System_Files/system.global.app.include.ui.php'; ?>
+
+<?php include './src/avakasaya/space6.php'; ?>
+<?php include './System_Files/system.app.clock.back.button.php'; ?>
+
+<?php include './src/avakasaya/space2.php'; ?>=
+<?php include './src/avakasaya/space16.php'; ?>
+<?php include './src/avakasaya/space10.php'; ?>
+<?php include './src/avakasaya/space10.php'; ?>
+<?php include './System_Files/system.app.clock.content.php'; ?>
